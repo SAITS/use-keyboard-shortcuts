2fa0461c489e85bd904b6b9d778ee253dc677fa5
Jimmy <jimhog2@outlook.com>
2019-07-09 15:03:43 +0200

    Initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,67 @@
-import React, { Component } from 'react'
+import React, { useState } from "react"
 
-import ExampleComponent from 'use-keyboard-shortcuts'
+import useKeyboardShortcuts from "use-keyboard-shortcuts"
 
-export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
-    )
+const onAlert = event => alert("ctrl + a was clicked")
+
+const App = () => {
+  const [ctrlScrollIsActive, setCtrlScrollIsActive] = useState(false)
+  const [rangeValue, setRangeValue] = useState(10)
+
+  const handleCtrlScroll = event => {
+    const movementX = event.movementX
+    if (movementX > 0 && rangeValue < 20) return setRangeValue(rangeValue + 1)
+    if (movementX < 0 && rangeValue > 1) return setRangeValue(rangeValue - 1)
   }
+
+  useKeyboardShortcuts([{ keys: ["ctrl", "a"], onEvent: onAlert }])
+  useKeyboardShortcuts(
+    [{ keys: ["ctrl", "shift", "scroll"], onEvent: handleCtrlScroll }],
+    ctrlScrollIsActive,
+    [rangeValue],
+    "mousewheel"
+  )
+
+  return (
+    <main>
+      <header>
+        <h1>Examples for useKeyboardShortcuts</h1>
+      </header>
+      <section className="content">
+        <div className="example-container">
+          <span>
+            Press <code>ctrl/cmd + a</code> to trigger an alert
+          </span>
+        </div>
+        <div />
+        <div className="example-container">
+          <label htmlFor="ctrlScroll">
+            <span>
+              Check this box to enable <code>ctrl + shift + scroll</code>
+            </span>
+            <input
+              type="checkbox"
+              className="checkbox"
+              name="ctrlScroll"
+              checked={ctrlScrollIsActive}
+              onChange={() => setCtrlScrollIsActive(!ctrlScrollIsActive)}
+            />
+          </label>
+          <span>
+            Try holding down <code>ctrl + shift</code> and move the mouse wheel
+          </span>
+          <input
+            type="range"
+            max={20}
+            min={1}
+            value={rangeValue}
+            className="range-slider"
+            onChange={e => setRangeValue(Number(e.target.value))}
+          />
+        </div>
+      </section>
+    </main>
+  )
 }
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,22 +1,78 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import { useEffect } from "react"
 
-import styles from './styles.css'
+const ALLOWED_COMBO_KEYS = JSON.stringify(["ctrl", "shift", "alt"])
+const ALLOWED_EVENTS = JSON.stringify(["keydown", "mousewheel"])
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
+const throwError = message =>
+  console.error(`Error thrown for useKeyboardShortcuts: ${message}`)
 
-  render() {
-    const {
-      text
-    } = this.props
+const allComboKeysPressed = (keys, event) => {
+  if (keys.includes("ctrl") && (!event.ctrlKey && !event.metaKey)) return false
+  if (keys.includes("shift") && !event.shiftKey) return false
+  if (keys.includes("alt") && !event.altKey) return false
+  return true
+}
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+const validateNonComboKeys = keys => {
+  if (!keys.length)
+    return `You need to specify a key besides ${ALLOWED_COMBO_KEYS}`
+  if (keys.length > 1)
+    return `Only one key besides ${ALLOWED_COMBO_KEYS} can be used.
+    Found ${keys.length}: [${keys.map(key => `"${key}"`)}]`
+}
+
+const withoutComboKeys = key => !JSON.parse(ALLOWED_COMBO_KEYS).includes(key)
+
+const generateFunction = (shortcut, event) => {
+  if (!event) return throwError("No event passed to the function")
+
+  const nonComboKeys = shortcut.keys.filter(withoutComboKeys)
+
+  const error = validateNonComboKeys(nonComboKeys)
+  if (error) return throwError(error)
+
+  const nonComboKey = nonComboKeys[0]
+
+  const shouldExecAction =
+    allComboKeysPressed(shortcut.keys, event) &&
+    (nonComboKey === "scroll" || nonComboKey === event.key) &&
+    !shortcut.disabled
+
+  if (shouldExecAction) {
+    event.preventDefault()
+    return shortcut.onEvent(event)
   }
 }
+
+const useKeyboardShortcuts = (
+  shortcuts,
+  active = true,
+  dependencies = [],
+  eventType = "keydown"
+) => {
+  if (!shortcuts || !shortcuts.length)
+    return throwError("You need to pass at least one shortcut as an argument")
+
+  if (!ALLOWED_EVENTS.includes(eventType))
+    return throwError(
+      `Unsupported event. Supported events are: ${ALLOWED_EVENTS}. Found event: "${eventType}"`
+    )
+
+  const handleKeyboardShortcuts = e =>
+    shortcuts.forEach(shortcut => generateFunction(shortcut, e))
+
+  const addEventListener = () =>
+    document.addEventListener(eventType, handleKeyboardShortcuts, {
+      passive: false,
+    })
+
+  const removeEventListener = () =>
+    document.removeEventListener(eventType, handleKeyboardShortcuts)
+
+  useEffect(() => {
+    active ? addEventListener() : removeEventListener()
+    return removeEventListener
+  }, [active, ...dependencies])
+}
+
+export default useKeyboardShortcuts
